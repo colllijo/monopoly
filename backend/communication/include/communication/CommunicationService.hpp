c20f0211db947c32dcc84bd75800eb322ed31170
Liam Metzger <liam.metzger@bluewin.ch>
2024-09-23 10:44:20 +0200

    Improve sending commands and receving responses

--- backend/communication/include/communication/CommunicationService.hpp
+++ backend/communication/include/communication/CommunicationService.hpp
@@ -1,14 +1,19 @@
 #pragma once
 
+#include <cstdint>
 #include <functional>
 #include <memory>
 #include <nlohmann/json.hpp>
 #include <string>
+#include <string_view>
 #include <thread>
 #include <event2/event.h>
 
 #include <amqpcpp.h>
 #include <amqpcpp/libevent.h>
+
+#include <unordered_set>
+#include <uuid/uuid.h>
 
 #include "Command.hpp"
 #include "ThreadSafeMap.hpp"
@@ -35,7 +40,18 @@ private:
 	std::unique_ptr<AMQP::TcpConnection> connection;
 	std::unique_ptr<AMQP::TcpChannel> channel;
 
+	std::unordered_set<std::string_view> requests;
 	ThreadSafeMap<std::string, nlohmann::json> responses;
 
-	void declareQueues();
+	void onMessage(const AMQP::Message& message, uint64_t deliveryTag, bool redelivered);
+
+	std::string createCorrelationId() const {
+		uuid_t uuid;
+		char uuid_str[37];
+
+		uuid_generate(uuid);
+		uuid_unparse(uuid, uuid_str);
+
+		return std::string(uuid_str);
+	};
 };
--- backend/communication/src/CommunicationService.cpp
+++ backend/communication/src/CommunicationService.cpp
@@ -3,31 +3,32 @@
 #include <amqpcpp/flags.h>
 #include <amqpcpp/linux_tcp/tcpconnection.h>
 
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <optional>
 #include <string>
+
 #include "nlohmann/json_fwd.hpp"
 
 CommunicationService::CommunicationService()
-	: base(event_base_new(), event_base_free)
-	, handler(AMQP::LibEventHandler(base.get()))
+    : base(event_base_new(), event_base_free), handler(AMQP::LibEventHandler(base.get()))
 {
 	if (!base) throw std::runtime_error("Failed to create event base");
 
 	connection = std::make_unique<AMQP::TcpConnection>(&handler, AMQP::Address("amqp://guest:guest@localhost"));
 	channel = std::make_unique<AMQP::TcpChannel>(connection.get());
 
-	declareQueues();
+	// Declare queues
+	channel->declareQueue("game_queue");
+	channel->declareQueue("data_queue");
 
 	std::cout << "Connected to RabbitMQ" << std::endl;
 };
 
 void CommunicationService::start()
 {
-	communicationThread = std::thread([this]() {
-		event_base_dispatch(base.get());
-	});
+	communicationThread = std::thread([this]() { event_base_dispatch(base.get()); });
 }
 
 void CommunicationService::stop()
@@ -41,32 +42,59 @@ void CommunicationService::stop()
 	communicationThread.detach();
 }
 
-void CommunicationService::declareQueues()
-{
-	channel->declareQueue("game_queue");
-	channel->declareQueue("data_queue");
-}
-
-nlohmann::json CommunicationService::execute(const amqp::Command& command)
+nlohmann::json CommunicationService::execute(const amqp::Command &command)
 {
 	nlohmann::json json = command;
 	std::string payload = json.dump();
+	std::string correlationId = createCorrelationId();
+	std::string queue;
 
-	channel->declareQueue(AMQP::exclusive).onSuccess([this, payload](const std::string &name, int, int) {
-		AMQP::Envelope msg(payload.c_str(), payload.size());
-		msg.setReplyTo(name);
+	switch (command.queue) {
+		case amqp::CommandQueue::GAME:
+			queue = "game_queue";
+			break;
+		case amqp::CommandQueue::DATA:
+			queue = "data_queue";
+			break;
+	}
 
-		channel->publish("", "data_queue", msg);
-		channel->consume(name).onReceived([this](const AMQP::Message &message, uint64_t, bool) {
-			std::string response(message.body(), message.bodySize());
-			std::cout << "Received: " << response << std::endl;
 
-			responses.put("games", nlohmann::json::parse(response));
-		});
-	});
+	channel->declareQueue(AMQP::exclusive)
+	    .onSuccess(
+	        [this, correlationId, payload, queue](const std::string &name, int, int)
+	        {
+		        AMQP::Envelope msg(payload.c_str(), payload.size());
+		        msg.setReplyTo(name);
+		        msg.setCorrelationID(correlationId);
 
-	std::optional<nlohmann::json> response = responses.get("games");
+		        requests.insert(correlationId);
+
+		        channel->publish("", queue, msg);
+		        channel->consume(name).onReceived(std::bind(&CommunicationService::onMessage, this,
+		                                                    std::placeholders::_1, std::placeholders::_2,
+		                                                    std::placeholders::_3));
+	        });
+
+	std::optional<nlohmann::json> response = responses.get(correlationId);
 
 	if (response.has_value()) return response.value();
 	return nullptr;
+}
+
+void CommunicationService::onMessage(const AMQP::Message &message, uint64_t, bool)
+{
+	if (!message.hasCorrelationID() || !requests.contains(message.correlationID())) return;
+
+	std::string correlationId = message.correlationID();
+	std::string response(message.body(), message.bodySize());
+
+	try {
+		responses.put(message.correlationID(), nlohmann::json::parse(response));
+	} catch (const nlohmann::json::parse_error &e) {
+		std::cerr << "Failed to parse response for " << correlationId << ": " << e.what() << std::endl;
+		responses.put(message.correlationID(), nullptr);
+	}
+
+	requests.erase(message.correlationID());
+	channel->removeQueue(message.routingkey());
 }
